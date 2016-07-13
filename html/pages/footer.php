137b0f3f7d77bbcd9fbe96cbb7b91a40df2474fa
Demjan <Demjan>
2016-07-13 13:48:57 +0300

    project init

---
+++ html/pages/footer.php
@@ -0,0 +1,6 @@
+  <footer class="timeout3">
+    <div class="timeout1"></div>
+    <div class="timeout2"></div>
+  </footer>
+</body>
+</html>
---
+++ html/pages/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>PHP</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../css/madia.css">
+		<link rel="stylesheet" href="../css/main.css">
+		<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+  <div class="topnav_fixed_block">
+    <section>
+      <logohere></logohere>
+      <nav class="top-nav">
+        <menu>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+        </menu>
+      </nav>
+    </section>
+  </div>
